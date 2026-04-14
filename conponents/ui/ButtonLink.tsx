ef13ebdad4787464c02566e57d6a78070ca075b2
Anton Sysuev <anton@MacBook-Air-Anton.local>
2026-04-14 11:51:31 +0300

    feat: initial commit

---
+++ conponents/ui/ButtonLink.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/Button"
+export function ButtonLink() {
+    return (
+        <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+        <Button>Открыть Google</Button>
+    </a>
+)
+}
